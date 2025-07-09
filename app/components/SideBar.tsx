eb9562bc4abf304cb14881c155bcb2bd07a156b7
Helal Uddin -SWE <seoguro1999@gmail.com>
2025-07-09 23:18:19 +0600

    4th commit for update dashboard

--- app/components/SideBar.tsx
+++ app/components/SideBar.tsx
@@ -1,23 +1,44 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 import Link from 'next/link';
-import { Settings } from 'lucide-react';
+import { ChartColumnStacked, House, Settings, Ticket } from 'lucide-react';
+
+// import { NavLink } from 'react-router';
 
 const SidebarElements = [
   {
     label: 'Overview',
     url: '/dashboard',
+    icon: <House />,
   },
-  { label: 'Customer', url: '/dashboard/customers' },
-	{ label: 'Invoices', url: '/dashboard/invoices' },
-	{label:"Settings",url:"/dashboard", icon:<Settings/>}
+  {
+    label: 'Customer',
+    url: '/dashboard/customers',
+    icon: <ChartColumnStacked />,
+  },
+  { label: 'Invoices', url: '/dashboard/invoices', icon: <Ticket /> },
+  { label: 'Settings', url: '/dashboard', icon: <Settings /> },
 ];
 const SideBar = () => {
   return (
     <div>
       <ul>
         {SidebarElements.map((link) => (
-          <li key={link.label} className="list-none text-2xl">
-            <Link href={link.url}>{link.label}</Link>
+          <li
+            key={link.label}
+            className="list-none cursor-pointer font-bold hover:bg-gray-600 hover:text-white ml-3 p-3 text-2xl"
+          >
+            <div className="flex gap-2 ">
+              <Link href={link.url}>
+                <label className="">{link.icon}</label>
+              </Link>
+              <Link href={link.url}>
+                <label className="hidden  md:block">
+                  {link.label}
+                  
+                </label>{' '}
+              </Link>
+            </div>
           </li>
         ))}
       </ul>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Dashboard from './dashboard/page'
 
 const Home = () => {
   return (
     <div>
-      thid Home
+     <Dashboard/>
     </div>
   )
 }
